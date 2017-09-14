dcb856fb4de69455560c35b8c46abcaa11a6fc42
Douglas <dougiechong@gmail.com>
2017-09-14 08:30:55 -0400

    changed home screen

--- client/src/App.js
+++ client/src/App.js
@@ -24,10 +24,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to MyHeatMaps</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload {this.state.password}.
+          To get started, log into strava.
         </p>
       </div>
     );
