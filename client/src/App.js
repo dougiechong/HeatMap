bd0a400788107ef7ee560cbd48a2be71d150bb93
Douglas <dougiechong@gmail.com>
2017-09-29 13:53:06 -0400

    updated some styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var styles = {
+    appHeader: {
+      color: 'white',
+      backgroundColor: '#222',
+      height: '150px',
+      padding: '20px'
+    },
+
+    appIntro: {
+      fontSize: 'large'
+    },
+
+    app: {
+      textAlign: 'center'
+    }
+};
+
 class App extends Component {
 
   // Initialize state
-  state = { password: "yo" }
+  /*state = { password: "yo" }
 
   // Fetch passwords after first mount
   componentDidMount() {
@@ -17,16 +34,16 @@ class App extends Component {
     fetch('/api/string')
       .then(res => res.json())
       .then(password => this.setState({password}));
-  }
+  }*/
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className="App" style={styles.app}>
+        <div className="App-header" style={styles.appHeader}>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to MyHeatMaps</h2>
         </div>
-        <p className="App-intro">
+        <p className="App-intro" style={styles.appIntro}>
           To get started, log into strava.
         </p>
       </div>
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  renderIntoDocument,
+  findRenderedDOMComponentWithClass
+} from 'react-addons-test-utils';
+import {expect} from 'chai';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('checks for app header text', () => {
+  const component = renderIntoDocument(<App/>);
+  const appHeader = findRenderedDOMComponentWithClass(component, 'App-header');
+  expect(appHeader).to.be.ok;
+  const appHeaderText = appHeader.textContent;
+  expect(appHeaderText).to.equal('Welcome to MyHeatMaps');
 });
