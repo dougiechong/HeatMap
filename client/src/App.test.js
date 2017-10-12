3fac3fd20cf6d779189ab6d8dce9d1732cc5e889
Douglas <dougiechong@gmail.com>
2017-10-12 10:34:30 -0400

    wrote failing tests for app header text and app header style

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,7 +4,9 @@ import {
   renderIntoDocument,
   findRenderedDOMComponentWithClass
 } from 'react-addons-test-utils';
-import {expect} from 'chai';
+//import {expect} from 'chai';
+const {expect} = require('chai')
+  .use(require('chai-style'))
 import App from './App';
 
 
@@ -18,4 +20,14 @@ it('checks for app header text', () => {
   const appHeader = findRenderedDOMComponentWithClass(component, 'App-header');
   expect(appHeader).to.be.ok;
   const appHeaderText = appHeader.textContent;
+  expect(appHeaderText.text()).to.have.string('Some text');
+});
+
+//TODO: make reusable function to check styles
+it('checks for app header styles', () => {
+  const component = renderIntoDocument(<App/>);
+  const appHeader = findRenderedDOMComponentWithClass(component, 'App-header');
+  expect(appHeader).to.be.ok;
+  expect(appHeader).to.have.style('backgroundColor', '#222');
+  expect(appHeader).to.have.style('color', 'blue');
 });
