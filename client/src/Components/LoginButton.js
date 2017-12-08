d2c1588f089c187b0d65914d915164e26917dfa0
Douglas <dougiechong@gmail.com>
2017-12-08 18:13:07 -0500

    fixed redirect links for prod

--- client/src/Components/LoginButton.js
+++ client/src/Components/LoginButton.js
@@ -7,19 +7,19 @@ export default class LoginButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      link: "https://stravaheatmaps.herokuapp.com/login/strava"
+      link: "https://www.strava.com/oauth/authorize?client_id=20778&response_type=code&redirect_uri=https://stravaheatmaps.herokuapp.com&scope=write&state=mystate&approval_prompt=force"
     };
   }
 
   componentDidMount() {
-    if(process.env.NODE_ENV === "development"){
-      this.setState({link :"http://localhost:5000/login/strava"});
+    if(process.env.NODE_ENV == "development"){
+      this.setState({link :"https://www.strava.com/oauth/authorize?client_id=20778&response_type=code&redirect_uri=http://localhost:3000&scope=write&state=mystate&approval_prompt=force"});
     } 
   }
 
   render() {
     return (
-      <a className = "Login-button" href={"https://www.strava.com/oauth/authorize?client_id=20778&response_type=code&redirect_uri=http://localhost:3000&scope=write&state=mystate&approval_prompt=force"}>
+      <a className = "Login-button" href={this.state.link}>
         <button>
           Login
         </button>
